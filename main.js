61a9ad99c33f4f392d0ee922c1bdd1233fbd388c
creeperdr <drewday31@gmail.com>
2025-10-17 23:46:30 -0500

    Site Layout
    Created Formatted Empty WIP Pages

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ let collapse = document.querySelectorAll("#header .collapse");
 toggle.addEventListener('click',function(){
     collapse.forEach(col=>col.classList.toggle("collapse-toggle"))
 })
+
+// Sticky Navigation
+window.onscroll = function(){myFunction()};
+
+// get current value
+let navbar = document.getElementById("header");
+
+// get the navbar position
+let sticky = navbar.offsetTop;
+
+// sticky function
+function myFunction(){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
