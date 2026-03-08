8eaa1d5f62a235be5512e7c85592ba19c69261e0
creeperdr <drewday31@gmail.com>
2026-03-08 00:01:10 -0600

    Header & Footer SSoT
    Created header.html and footer.html files so that I do not have to edit the header and footer of every single page.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// --- Auto Add Header and Footer to every HTML page ---
+async function injectLayout() {
+    // Inject Header
+    const headerRes = await fetch('header.html');
+    const headerHtml = await headerRes.text();
+    document.getElementById('header-placeholder').innerHTML = headerHtml;
+
+    // Inject Footer
+    const footerRes = await fetch('footer.html');
+    const footerHtml = await footerRes.text();
+    document.getElementById('footer-placeholder').innerHTML = footerHtml;
+}
+
+document.addEventListener('DOMContentLoaded', injectLayout);
+
+//Collapse Menu
 let toggle = document.querySelector("#header .toggle-button");
 let collapse = document.querySelectorAll("#header .collapse");
 
